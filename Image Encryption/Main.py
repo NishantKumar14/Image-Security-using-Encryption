d3f2ba3403cc0c4b759e76c766b271855362fefa
Nishant Kumar <nk534031@gmail.com>
2022-02-12 23:48:40 +0530

    Update Main.py

--- Image Encryption/Main.py
+++ Image Encryption/Main.py
@@ -12,21 +12,23 @@ def encrypt_image():
     # Opening file in jpg, jpeg or png format
     file1 = filedialog.askopenfile(mode='r', filetypes=[('jpg', '*.jpg'), ('jpeg', '*.jpeg'), ('png', '*.png')])
     if file1 is not None:
-        # print(file1) # -> For checking
+        # print(file1) # -> For checking that what is type of variable of file1
         file_name = file1.name
-        # print(file_name) # -> For checking
-        key = entry1.get(1.0, END)
-        print(file_name, key)
-
-        specific_file = open(file_name, 'rb')  # Selecting file which we have to encrypt and decrypt
-        image = specific_file.read()  # Reading selected file
+        # print(file_name) # -> For checking that we are getting the right data for further operations
+        
+        key = entry1.get(1.0, END) # Extracting the key that is enter by the user 
+        print(file_name, key) 
+        
+        # Extracting data from a specific file that have path of the selected image  
+        specific_file = open(file_name, 'rb')  # And using rb method that will reading data in byte formate
+        image = specific_file.read()  # Storing all the data in the image variable
         specific_file.close()
 
-        image = bytearray(image)
-        # XOR
-        for index, value in enumerate(image):
+        image = bytearray(image) # Converting pixel data into bytearray
+        # XOR 
+        for index, value in enumerate(image): # Enumerate methode will provide all value of data that store in image variable
             image[index] = value ^ int(key)
-        file = open(file_name, 'wb')
+        file = open(file_name, 'wb') # Saving the file after writing the value
         file.write(image)
         file.close()
 
